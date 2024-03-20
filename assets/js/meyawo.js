4d1e33a8a555bbc58894dd63ed3f3861beca1790
skandar1999 <sabriskandar3@gmail.com>
2024-03-20 01:14:33 +0100

    Updated with carousel items

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,18 +1,4 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
 
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-// smooth scroll
 $(document).ready(function(){
     $(".navbar .nav-link").on('click', function(event) {
 
@@ -36,3 +22,28 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+
+$(document).ready(function() {
+    let certCarousel = document.querySelector('.cert-carousel');
+    let certItems = document.querySelectorAll('.cert-item');
+    let scrollDistance = certItems[0].offsetWidth + 5; 
+    let currentCertIndex = 0;
+
+    function scrollNext() {
+        currentCertIndex++;
+        if (currentCertIndex >= certItems.length) {
+            currentCertIndex = 0;
+        }
+        certCarousel.style.transform = 'translateX(-' + (scrollDistance * currentCertIndex) + 'px)';
+    }
+
+    setInterval(scrollNext, 2100); 
+
+    window.addEventListener('resize', function() {
+        scrollDistance = certItems[0].offsetWidth + 5; 
+        certCarousel.style.transform = 'translateX(-' + (scrollDistance * currentCertIndex) + 'px)';
+    });
+});
+
+
